7df38a8790da44ea3852422618ddfa7f75b86015
DESKTOP-ECEVRKH\mio48 <mio4810@naver.com>
2024-11-03 22:39:48 +0900

    [이종혁] - SpringTest 생성

---
+++ src/main/java/com/vamos/spring/test/SpringTest1Application.java
@@ -0,0 +1,13 @@
+package com.vamos.spring.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTest1Application.class, args);
+	}
+
+}
